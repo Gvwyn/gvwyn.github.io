a3217c18778d70156c674acd58ac8a2d610dcf35
Gvwyn <kovacstormabalazskrisztofer@gmail.com>
2023-12-07 01:53:23 +0100

    tweaks

--- js/main.js
+++ js/main.js
@@ -6,27 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
 function DisplaySnow() {
     const starContainer = document.body;
 
-    for (let i = 0; i < 150; i++) {
+    for (let i = 0; i < 50; i++) {
         const star = document.createElement("div");
         star.className = "snow";
-        star.style.left = Math.random() * window.innerWidth + "px";
+        star.style.left = Math.random() * window.innerWidth*1.5 + "px";
         star.style.animationDuration = (Math.random() * 2 + 1) + "s";
         starContainer.appendChild(star);
     }
 }
 
+/* dark/light mode switch */
 function toggleBackground() {
     const toggleIcon = document.getElementById('toggle-icon');
+    const logo = document.getElementById('logo');
+    const expand = document.getElementById('expand');
 
     if (document.body.classList.contains('light')) {
         document.body.classList.remove('light');
+        logo.src = 'images/icon.png';
+        expand.src = 'images/expand.svg';
+        document.documentElement.style.setProperty('--line', '#FFF');
         toggleIcon.src = 'images/night.svg';
     } else {
         document.body.classList.add('light');
+        logo.src = 'images/light/icon.png';
+        expand.src = 'images/light/expand.svg';
+        document.documentElement.style.setProperty('--line', '#EBF3E8');
         toggleIcon.src = 'images/sun.svg';
     }
 }
 
+/* hamburger menu functions */
 function HamburgerON() {
     var menu = document.getElementById('hamburger');
     menu.style.display = 'block';
